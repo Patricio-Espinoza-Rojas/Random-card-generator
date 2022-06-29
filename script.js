fb89fa5451ecc7210f0f3550200853b1c1fb6dcb
Patricio-Espinoza-Rojas <p.alfonsoespinoza@gmail.com>
2022-06-29 20:20:50 +0000

    updating all files

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-    let cards = ['♠','♣','♥','♦'];
-    
-    let numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+let cards = ['♠', '♣', '♥', '♦'];
+
+let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
 function randomCardIndex() {
-   return Math.floor(Math.random() * cards.length);
+    return Math.floor(Math.random() * cards.length);
 }
 
-    let randomCard = cards [randomCardIndex()]
+let randomCard = cards[randomCardIndex()]
 
 function randomNumberIndex() {
-    return Math.floor(Math.random() * numbers.length); 
+    return Math.floor(Math.random() * numbers.length);
 }
-    let randomNumbers = numbers [randomNumberIndex()]
+let randomNumbers = numbers[randomNumberIndex()]
 
 
-
-    const topSuit = document.querySelector(".top-suit");
+const topSuit = document.querySelector(".top-suit");
 if (randomCard == "♥") {
-  topSuit.style.color = "red";
+    topSuit.style.color = "red";
 }
-    if (randomCard == "♦") {
-        topSuit.style.color = "red";
+if (randomCard == "♦") {
+    topSuit.style.color = "red";
 }
 topSuit.innerHTML = randomCard;
 
 
 const BottomSuit = document.querySelector(".bottom-suit");
 if (randomCard == "♥") {
-  BottomSuit.style.color = "red";
+    BottomSuit.style.color = "red";
 }
-    if (randomCard == "♦") {
-        BottomSuit.style.color = "red";
+if (randomCard == "♦") {
+    BottomSuit.style.color = "red";
 }
 BottomSuit.innerHTML = randomCard;
 
