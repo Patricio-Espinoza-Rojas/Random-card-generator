b25227f059b7c46a4920cfbd505cc65fb0d81993
Patricio-Espinoza-Rojas <p.alfonsoespinoza@gmail.com>
2022-06-29 20:12:48 +0000

    updating all files

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
     let numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 
 function randomCardIndex() {
-    Math.floor(Math.random() * cards.length);
+   return Math.floor(Math.random() * cards.length);
 }
 
     let randomCard = cards [randomCardIndex()]
 
 function randomNumberIndex() {
-    Math.floor(Math.random() * numbers.length); 
+    return Math.floor(Math.random() * numbers.length); 
 }
     let randomNumbers = numbers [randomNumberIndex()]
 
@@ -35,46 +35,6 @@ if (randomCard == "♥") {
 BottomSuit.innerHTML = randomCard;
 
 
-let numberSeven = document.querySelector('.seven')
-numberSeven.innerHTML = randomNumbers;  
+let numberSeven = document.querySelector('.seven');
+numberSeven.innerHTML = randomNumbers;
 
-
-
-    // document.getElementById("showing").innerHTML = cards[Cards_] + " " + numbers[Numbers];
-    // document.getElementById(".topSuit").innerHTML = cards[Cards_] + " " + numbers[Numbers];
-    // document.getElementById(".BottomSuit").innerHTML = cards[Cards_] + " " + numbers[Numbers];
-
-
-
-
-
-// function displayCard() {
-//     let singleCardSpade1 = cardSpade1[Math.floor(Math.random() * cardSpade1.length)];
-//     let singleCardSpade2 = cardSpade2[Math.floor(Math.random() * cardSpade2.length)];
-//     let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
-//     let showCard = `${singleCardSpade1} ${singleNumber} ${singleCardSpade2}`;
-    // document.getElementById("cardsRed").script.background = '#FF0000';
-    
-     
-// }
-
-
-
-
-// window.onload = () => {
-//     document.querySelector('.card').classList.add(generateRandomSuit());
-//     document.querySelector('.card').innerHTML = (generateRandomSuit());
-// };
-
-
-// let generateRandomNumber = () => {
-//     let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K",];
-//     let indexNumbers = Math.floor(Math.random() * numbers.length);
-//     return numbers[indexNumbers];
-// }
-
-// let generateRandomSuit = () => {
-// let suit = ["diamond","spade","heart","club"];
-// let indexSuit = Math.floor(Math.random() * suit.length);
-// return numbers[indexSuit];
-// }
